d860fc9380df791e4c49288512fe3ad023625476
AlvinSheng <asheng1067@gmail.com>
2022-02-17 12:53:04 -0500

    combining all aggregated data from 2019-2021 September 30

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -16,3 +16,17 @@ orders2020_agg_received <- orders2020_df %>%
 # colSums(orders2020_df[orders2020_df$`Date Received` == as.Date("2020-12-08"), 14:34])
 
 saveRDS(orders2020_agg_received, file = here("intermediary_data/orders2020_agg_received.rds"))
+
+
+
+
+orders2019_agg <- readRDS(here("intermediary_data/orders2019_agg_received.rds"))
+orders2020_agg <- readRDS(here("intermediary_data/orders2020_agg_received.rds"))
+orders2021_agg <- readRDS(here("intermediary_data/orders2021_agg_received.rds"))
+
+# Combine all datasets for 2019 up to 2021 September (ensuring all orders have been filled)
+orders_allyears_agg_received <- rbind(orders2019_agg, orders2020_agg, orders2021_agg[orders2021_agg$`Date Received` < as.Date("2021-10-01"), ])
+
+saveRDS(orders_allyears_agg_received, file = here("intermediary_data/orders_allyears_agg_received.rds"))
+
+
