56eb76ffab61a3614680a619686cb9fe2bee9dbc
AlvinSheng <asheng1067@gmail.com>
2022-04-27 13:32:41 -0400

    aggregating weekly

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -120,3 +120,58 @@ orders_allyears_agg_received <- orders_allyears_df %>%
 saveRDS(orders_allyears_agg_received, file = here("intermediary_data/orders_allyears_agg_received.rds"))
 
 
+
+##### 
+
+# Updating the data: aggregate weekly, include Yinqiao's feature engineering
+
+# Re-run the code in the last section, right before the aggregation
+
+girl_clothes <- c("gt", "gb", "gu", "gs", "gc")
+boy_clothes <- c("bt", "bb", "bu", "bs", "bc")
+woman_clothes <- c("wt", "wb", "wu", "ws", "wc")
+man_clothes <- c("mt", "mb", "mu", "ms", "mc")
+
+orders_allyears_df <- readRDS(here("intermediary_data/orders_allyears_df.rds"))
+
+orders_allyears_df <- orders_allyears_df[!(orders_allyears_df$POC %in% c("Clothing Exchange", "Emergency Clothing Events")), ]
+orders_allyears_df <- orders_allyears_df[orders_allyears_df$adult != 1 | is.na(orders_allyears_df$adult), ] # in 2019 data, NA counts as not adult
+
+# re-doing the zero-padding, in case the the above code removed some days
+orders_allyears_df <- orders_allyears_df %>% 
+  complete(`Date Received` = seq.Date(min(`Date Received`), max(`Date Received`), by = "day"))
+
+# re-creating the four indicator columns, for the zero-padded rows
+orders_allyears_df <- orders_allyears_df %>% mutate(female_small = (rowSums(orders_allyears_df[girl_clothes], na.rm = T) != 0), 
+                                                    male_small = (rowSums(orders_allyears_df[boy_clothes], na.rm = T) != 0), 
+                                                    female_large = (rowSums(orders_allyears_df[woman_clothes], na.rm = T) != 0), 
+                                                    male_large = (rowSums(orders_allyears_df[man_clothes], na.rm = T) != 0))
+
+
+
+# starting it on a Sunday, so a week interval will be Sunday to Saturday (to be consistent with Zoho data) 
+# so I omit data prior to "2019/01/20"
+
+orders_allyears_df <- orders_allyears_df[orders_allyears_df$`Date Received` >= as.Date("2019/01/20", "%Y/%m/%d"), ]
+
+weekno <- as.numeric(orders_allyears_df$`Date Received` - as.Date("2019/01/20", "%Y/%m/%d")) %/% 7
+orders_allyears_df$start_of_week <- as.Date("2019/01/20", "%Y/%m/%d") + 7 * weekno
+
+
+
+# aggregated by start_of_week by summing up any count variable
+# and taking the earliest Date Filled date
+orders_weekly_agg_received <- orders_allyears_df %>% 
+  group_by(start_of_week) %>%
+  summarise(earliest_filled = min(`Date Filled`, na.rm = T), 
+            latest_filled = max(`Date Filled`, na.rm = T),
+            mean_filled = mean(`Date Filled`, na.rm = T),
+            mean_lag = mean(`Date Filled` - `Date Received`, na.rm = T),
+            num_requests = sum(female_small + male_small + female_large + male_large),
+            across(10:38, sum, na.rm = T))
+
+# checking specific dates
+# colSums(orders_allyears_df[orders_allyears_df$start_of_week == as.Date("2020-12-06"), 10:38], na.rm = T)
+
+saveRDS(orders_weekly_agg_received, file = here("intermediary_data/orders_allyears_agg_received.rds"))
+
